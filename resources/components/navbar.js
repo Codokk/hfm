1923258f3cec5d343a28bc7082e23378eeaa8999
S4UCE11 <austineckenrod@outlook.com>
2024-02-03 20:21:41 -0500

    templated navbar

--- resources/components/navbar.js
+++ resources/components/navbar.js
@@ -4,5 +4,17 @@ export default {
     const count = ref(0)
     return { count }
   },
-  template: `<div>count is {{ count }}</div>`
+  template: `
+  <div class="surface-50 flex flex-row align-items-center justify-content-between">
+    <div class="inline-block w-2 h-1">
+      <button class="grey-800 flex flex-row align-items-center inline-block">
+        <img src="https://placehold.co/400" class="mr-1 w-3">
+        <p> New entry </p>
+      </button>
+    </div>
+
+    <div>
+      <p>these are others</p>
+    </div>
+  </div>`
 }
